779785aea6b4e6d0779ee58ed13180a10eb29a80
eastcirclek870 <eastcirclek@sk.com>
2020-04-16 23:47:14 +0900

    Bump up Flink version to 1.10

--- stdout.sh
+++ stdout.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 scala \
-    -cp build/libs/session-generator-1.0-SNAPSHOT-all.jar \
-    com.github.sessiongen.Generator \
+    -cp build/libs/session-generator-0.1-all.jar \
+    com.github.sessiongen.generator.Generator \
     --rate 1000 \
     --interval 60 \
-    --total 600000 \
     --logout-probability 0.2 \
     --payload-size 20 \
     --running-time 3600 \
